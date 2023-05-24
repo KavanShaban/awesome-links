7227d62a38ad059bcbe27925f0fbca3bb324e1be
Kavan Shaban <KavanShaban@users.noreply.github.com>
2023-05-24 05:17:49 +0000

    added plugin-relay

--- graphql/builder.ts
+++ graphql/builder.ts
@@ -4,25 +4,28 @@
 import SchemaBuilder from "@pothos/core";
 import PrismaPlugin from '@pothos/plugin-prisma';
 import type PrismaTypes from '@pothos/plugin-prisma/generated';
+import RelayPlugin from '@pothos/plugin-relay';
 import prisma from "../lib/prisma";
 
 // 2. 
 export const builder = new SchemaBuilder<{
-  // 3. 
-  PrismaTypes: PrismaTypes
+    // 3. 
+    PrismaTypes: PrismaTypes
 }>({
-  // 4.
-  plugins: [PrismaPlugin],
-  prisma: {
-    client: prisma,
-  }
+    // 4.
+    //   plugins: [PrismaPlugin],
+    plugins: [PrismaPlugin, RelayPlugin],
+    relayOptions: {},
+    prisma: {
+        client: prisma,
+    }
 })
 
 // 5. 
 builder.queryType({
-  fields: (t) => ({
-    ok: t.boolean({
-      resolve: () => true,
+    fields: (t) => ({
+        ok: t.boolean({
+            resolve: () => true,
+        }),
     }),
-  }),
 });
--- graphql/types/Links.ts
+++ graphql/types/Links.ts
@@ -2,25 +2,28 @@
 import { builder } from "../builder";
 
 builder.prismaObject('Link', {
-  fields: (t) => ({
-    id: t.exposeID('id'),
-    title: t.exposeString('title'),
-    url: t.exposeString('url'),
-    description: t.exposeString('description'),
-    imageUrl: t.exposeString('imageUrl'),
-    category: t.exposeString('category'),
-    users: t.relation('User')
-  })
+    fields: (t) => ({
+        id: t.exposeID('id'),
+        title: t.exposeString('title'),
+        url: t.exposeString('url'),
+        description: t.exposeString('description'),
+        imageUrl: t.exposeString('imageUrl'),
+        category: t.exposeString('category'),
+        users: t.relation('User')
+    })
 })
- 
+
 // 1. 
 builder.queryField("links", (t) =>
-// 2. 
-  t.prismaField({
-    // 3. 
-    type: ['Link'],
-    // 4. 
-    resolve: (query, _parent, _args, _ctx, _info) =>
-      prisma.link.findMany({ ...query })
-  })
+    // 2. 
+    //   t.prismaField({
+    t.prismaConnection({
+        // 3. 
+        // type: ['Link'],
+        type: 'Link',
+        cursor: 'id',
+        // 4. 
+        resolve: (query, _parent, _args, _ctx, _info) =>
+            prisma.link.findMany({ ...query })
+    })
 )
